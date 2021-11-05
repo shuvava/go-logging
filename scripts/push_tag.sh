16e753fdf54ac71e8c0c0ce5bd77797757743cbd
shuvava <shuvava@users.noreply.github.com>
2021-11-05 07:44:28 -0700

    fix push_tag.sh script

--- scripts/push_tag.sh
+++ scripts/push_tag.sh
@@ -10,6 +10,12 @@ if [[ -z "${VERSION}" ]]; then
   echo "Usage: push_tag.sh <version>"
   exit 1
 fi
+PREVIOUS_VERSION=$( git describe --tags --always --abbrev=0 --match='v[0-9]*.[0-9]*.[0-9]*' 2> /dev/null | sed 's/^.//')
+
+sed -i "" "s/go-logging@v${PREVIOUS_VERSION}/go-logging@v${VERSION}/" README.md
+git add README.md
+git commit -m "bump version to v${VERSION}"
+git push
 
 git tag -a "v${VERSION}" -m "version ${VERSION}"
 git push origin "v${VERSION}"
